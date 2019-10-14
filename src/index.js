b8d0198dd0d5b4a0bcb7c25495ebed26ab4fd5d0
Alivarik <45916819+Alivarik@users.noreply.github.com>
2019-10-14 16:15:02 +0300

    fix: add find empty position method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function solveSudoku(matrix) {
-  // your solution
+function saveEmptyPositions(matrix) {
+    var emptyPosition = [];
+    for (var i = 0; i < matrix.length; i++) {
+        for (var j = 0; j < matrix(i).length; i++) {
+            if (matrix[i][j] === 0) {
+                emptyPosition.push([i, j]);
+            }
+        }
+    }
 }
+
+module.exports = function solveSudoku(matrix) {
+    // your solution
+};
